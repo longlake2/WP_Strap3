6a4492bde506ffbe58f55f0d2d0ebda081779c68
longlake2 <sites@emailhart.net>
2017-08-12 19:35:52 -0400

    Modified functions.php, Added js/dist and js/manifest.js

--- functions.php
+++ functions.php
@@ -119,8 +119,8 @@ add_action( 'widgets_init', 'wp_strap3_widgets_init' );
 function wp_strap3_scripts() {
 	wp_enqueue_style( 'wp_strap3-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'wp_strap3-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-
+	wp_enqueue_script( 'wp_strap3-js', get_template_directory_uri() . '/js/dist/scripts.min.js', array('jquery'), '1.0.0', true );
+	
 	wp_enqueue_script( 'wp_strap3-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
