6681786b8a125b284e8fbc3b9cbc4e4312a719f4
longlake2 <sites@emailhart.net>
2017-08-13 09:50:59 -0400

    moved and changed files (function and sass)

--- functions.php
+++ functions.php
@@ -96,44 +96,13 @@ function wp_strap3_content_width() {
 add_action( 'after_setup_theme', 'wp_strap3_content_width', 0 );
 
 /**
- * Register widget area.
- *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
+ * Implement scripts
  */
-function wp_strap3_widgets_init() {
-	register_sidebar( array(
-		'name'          => esc_html__( 'Sidebar', 'wp_strap3' ),
-		'id'            => 'sidebar-1',
-		'description'   => esc_html__( 'Add widgets here.', 'wp_strap3' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
-	) );
-}
-add_action( 'widgets_init', 'wp_strap3_widgets_init' );
-
+require get_template_directory() . '/inc/scripts.php';
 /**
- * Enqueue scripts and styles.
+ * Implement widgets
  */
-function wp_strap3_scripts() {
-	wp_enqueue_style( 'wp_strap3-style', get_stylesheet_uri() );
-
-	wp_enqueue_script( 'wp_strap3-js', get_template_directory_uri() . '/js/dist/scripts.min.js', array('jquery'), '1.0.0', true );
-	
-	wp_enqueue_script( 'wp_strap3-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
-}
-add_action( 'wp_enqueue_scripts', 'wp_strap3_scripts' );
-
-/**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
-
+require get_template_directory() . '/inc/widgets.php';
 /**
  * Custom template tags for this theme.
  */
